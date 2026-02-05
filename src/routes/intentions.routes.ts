8a55ba04f8946499a0a7c85efd61b3e47e013170
Rafael Oliveira <rafaelhenriqueoliveira1234@gmail.com>
2026-02-04 22:45:09 -0300

    Implementando documentação automática

--- src/routes/intentions.routes.ts
+++ src/routes/intentions.routes.ts
@@ -3,11 +3,30 @@ import { IntentionsController } from "../modules/intentions/controllers/intentio
 import { request } from "node:http";
 
 export async function intentionsRoutes(fastify: FastifyInstance, options: FastifyPluginOptions) {
-    fastify.post("", async (request: FastifyRequest, reply: FastifyReply) => {
+    fastify.post("", {
+        schema: {
+            body: {
+                type: "object",
+                properties: {
+                    zipcode_start: { type: "string" },
+                    zipcode_end: { type: "string" }
+                },
+            },
+        }
+    }, async (request: FastifyRequest, reply: FastifyReply) => {
         return new IntentionsController().post(request, reply);
     });
 
-    fastify.put("/:intentionId", async (request: FastifyRequest, reply: FastifyReply) => {
+    fastify.put("/:intentionId", {
+        schema: {
+            body: {
+                type: "object",
+                properties: {
+                    lead_id: {type: "string"}
+                },
+            },
+        },
+    }, async (request: FastifyRequest, reply: FastifyReply) => {
         return new IntentionsController().put(request, reply)
     });
 
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,35 @@
-import Fastify, { fastify } from "fastify"
+import Fastify from "fastify"
 import routes from "./routes/index.js"
 import cors from '@fastify/cors';
 import { errorHandler } from "./shared/errors/errorHandler.js";
+import swagger from "@fastify/swagger";
+import swaggerUI from "@fastify/swagger-ui";
 
 const app = Fastify({ logger: true })
 
 const start = async () => {
+
+    await app.register(swagger, {
+        swagger: {
+            info: {
+                title: "LeadsIntentionsApi",
+                description: "API para captar leads",
+                version: "1.0.0"
+            },        
+            host: "localhost:3333",
+            schemes: ["http"],
+            consumes: ["application/json"],
+            produces: ["application/json"]
+        }
+    });
+
+    await app.register(swaggerUI, {
+        routePrefix: "/docs",
+        uiConfig: {
+            docExpansion: "list",
+            deepLinking: false
+        }
+    });
 
     await app.register(cors);
     await app.register(routes);
